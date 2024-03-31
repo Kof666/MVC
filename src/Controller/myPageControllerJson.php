50cbdfa1a53d39b47e0836882c9f1ec266afe4b4
Kristoffer Berg <koffeberg@gmail.com>
2024-04-01 00:52:36 +0200

    created /api and /api/quote

---
+++ src/Controller/myPageControllerJson.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class myPageControllerJson
+{
+    // #[Route('/api/', name: 'api')]
+    // public function api(): Response
+    // {
+        
+    //     $data = [];
+
+    //     return $this->render('/api.html.twig', $data);
+    // }
+
+    #[Route('/api/quote')]
+    public function quote(): Response
+    {
+        // $quotes = [
+        //     "Code is like humor. When you have to explain it, it’s bad. – Cory House",
+        //     "Experience is the name everyone gives to their mistakes. – Oscar Wilde",
+        //     "Confusion is part of programming. ― Felienne Hermans"
+        // ]
+        $timestamp = time();
+        date_default_timezone_set('Europe/Amsterdam');
+        $date = date('Y-m-d', time());
+        $time = date('H:i:s', time());
+        $number = random_int(1, 3);
+
+        $quote = null;
+
+        if($number == 1) {
+            $quote = "Code is like humor. When you have to explain it, it’s bad. – Cory House";
+        } else if ($number == 2) {
+            $quote = "Experience is the name everyone gives to their mistakes. – Oscar Wilde";
+        } else if ($number == 3) {
+            $quote = "Confusion is part of programming. ― Felienne Hermans";
+        } 
+
+        $data = [
+            'quote' => $quote,
+            'date' => $date,
+            'time' => $time
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+}
