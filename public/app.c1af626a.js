cafb234dd241c6db65f75ca7df5baee4ed9f03ac
Kristoffer Berg <koffeberg@gmail.com>
2024-04-05 23:02:57 +0200

    fixed path to bth-server

---
+++ public/app.c1af626a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[524],{948:()=>{console.log("This log comes from assets/app.js - welcome to AssetMapper! 🎉"),console.log("This web-site is a part of the course MVC at BTH")}},s=>{var e;e=948,s(s.s=e)}]);
