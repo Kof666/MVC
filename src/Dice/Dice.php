fe17b55cddfa89064c477079c2bcae432cf0b241
Kristoffer Berg <koffeberg@gmail.com>
2024-04-06 04:03:49 +0200

    implemented exercise pig game

---
+++ src/Dice/Dice.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Dice;
+
+/**
+ * A simple dice
+ */
+class Dice
+{
+    /**
+     * @var integer value   the value of the dice
+     */
+    protected $value;
+
+    /**
+     * Constructor to initiate the dice
+     */
+    public function __construct()
+    {
+        $this->value = null;
+    }
+
+    /**
+     * Roll the dice
+     *
+     * @return integer that gives a value between one and six
+     */
+    public function roll()
+    {
+        $this->value = random_int(1, 6);
+        return $this->value;
+    }
+
+    /**
+     * Get value of the dice
+     *
+     * @return the value of the dice
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Value as string
+     *
+     * @return string value as string
+     */
+    public function getAsString()
+    {
+        return "[{$this->value}]";
+    }
+}
