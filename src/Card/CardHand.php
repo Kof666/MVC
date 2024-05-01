8a4548285105f9c91d09064fdfe5ccd8ed8e1923
Kristoffer Berg <koffeberg@gmail.com>
2024-05-02 00:02:14 +0200

    phpdoc installed

--- src/Card/CardHand.php
+++ src/Card/CardHand.php
@@ -4,35 +4,35 @@ namespace App\Card;
 
 class CardHand
 {
-    /**
-     * @var array<mixed, card> $card
-     */
-    private mixed $cards = array();
+//     /**
+//      * @var array<mixed, card> $card
+//      */
+//     private mixed $cards = array();
 
-    public function __construct()
-    {
-        $this->cards = [];
-    }
+//     public function __construct()
+//     {
+//         $this->cards = [];
+//     }
 
-    public function addCard(Card $card): void
-    {
-        $this->cards[] = $card;
-    }
+//     public function addCard(Card $card): void
+//     {
+//         $this->cards[] = $card;
+//     }
 
-    /**
-     * @return array<mixed, Card> $card
-     */
-    public function getCards(): mixed
-    {
-        return $this->cards;
-    }
+//     /**
+//      * @return array<mixed, Card> $card
+//      */
+//     public function getCards(): mixed
+//     {
+//         return $this->cards;
+//     }
 
-    public function getTotalValue(): int
-    {
-        $totalValue = 0;
-        foreach ($this->cards as $card) {
-            $totalValue += $card->getValue();
-        }
-        return $totalValue;
-    }
+//     public function getTotalValue(): int
+//     {
+//         $totalValue = 0;
+//         foreach ($this->cards as $card) {
+//             $totalValue += $card->getValue();
+//         }
+//         return $totalValue;
+//     }
 }
